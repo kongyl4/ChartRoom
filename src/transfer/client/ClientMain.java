220a88bbcfbd91e6111ca05bb76d01213a12814a
kongyl4 <310542914@qq.com>
2016-10-14 23:16:00 +0800

    multi broadcast

--- src/transfer/client/ClientMain.java
+++ src/transfer/client/ClientMain.java
@@ -2,6 +2,8 @@ package transfer.client;
 
 import java.io.IOException;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import transfer.common.Config;
 
@@ -9,20 +11,24 @@ import transfer.common.Config;
  * Created by kongyl4 on 2016/10/11.
  */
 public class ClientMain {
+
+    private static final ExecutorService inputThreadPool = Executors.newSingleThreadExecutor();
+    private static final ExecutorService outputThreadPool = Executors.newSingleThreadExecutor();
+
     public static void main(String[] args) {
         //while(true){
-        Socket socket=null;
-         try {
-            socket=new Socket("localhost",10002);
-             System.out.println(socket.getPort());
+        Socket socket = null;
+        try {
+            socket = new Socket(Config.HOST, Config.PORT);
+            outputThreadPool.execute(new ClientOutputThread(socket));
+            inputThreadPool.execute(new ClientInputThread(socket));
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            inputThreadPool.shutdown();
+            outputThreadPool.shutdown();
         }
-            ClientOutputThread clientOutputThread=new ClientOutputThread(socket);
-            Thread t1=new Thread(clientOutputThread);
-            t1.start();
-        Thread t2=new Thread(new ClientInputThread(socket));
-        t2.start();
+
         //}
 
     }
--- src/transfer/common/AbstractInputThread.java
+++ src/transfer/common/AbstractInputThread.java
@@ -1,8 +1,13 @@
 package transfer.common;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by wyx on 2016/10/14.
@@ -10,8 +15,20 @@ import java.net.Socket;
 public class AbstractInputThread implements Runnable {
     private final Socket socket;
 
+
+    private volatile boolean running = true;
+    LinkedList<Socket> list = new LinkedList<Socket>();
+
     public AbstractInputThread(Socket socket) {
         this.socket = socket;
+    }
+
+    public void addSocket(Socket socket) {
+        list.add(socket);
+    }
+
+    public void shutdown() {
+        running = false;
     }
 
     @Override
@@ -19,15 +36,40 @@ public class AbstractInputThread implements Runnable {
         DataInputStream is = null;
         try {
             is = new DataInputStream(socket.getInputStream());
-            String str=is.readUTF();
-            System.out.println(str);
+            while (running) {
+                String str = is.readUTF();
+                //1 asdasd
+                //dsadsa
+                if (!list.isEmpty()) {
+                    String[] arr = str.split("\\s+");
+                    int index = 0;
+                    if (arr.length == 2) {
+                        index = Integer.valueOf(arr[0]);
+                    }
+                    if (index == 0) {
+                        Iterator<Socket> it = list.iterator();
+                        while (it.hasNext()) {
+                            Socket socket = it.next();
+                            DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+                            os.writeUTF(str);
+                        }
+                    } else {
+                        Socket socket = list.get(index - 1);
+                        DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+                        os.writeUTF(str);
+                    }
+                }
+                System.out.println(str);
+            }
         } catch (IOException e) {
-            e.printStackTrace();
+            //e.printStackTrace();
+        } finally {
+            System.out.println("关闭输入连接");
             if (is != null) {
                 try {
                     is.close();
                 } catch (IOException e1) {
-                    e.printStackTrace();
+                    e1.printStackTrace();
                 }
             }
             if (socket != null) {
