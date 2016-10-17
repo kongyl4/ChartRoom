91bf8d1e1d2bc8cd6025bb5d3d5b352220ea8182
kongyl4 <310542914@qq.com>
2016-10-17 18:50:15 +0800

    multi broadcast

--- src/transfer/common/AbstractOutputThread.java
+++ src/transfer/common/AbstractOutputThread.java
@@ -3,26 +3,24 @@ package transfer.common;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-/**
- * Created by wyx on 2016/10/14.
- */
 public abstract class AbstractOutputThread implements Runnable {
     private final List<Socket> socketList = new LinkedList<Socket>();
+   // private final Map<String, Socket> mapList = new HashMap<String, Socket>();
+    private boolean running = true;
 
-    private volatile boolean running = true;
-
-    public void addSocket(Socket socket) {
+    public void addSocket( Socket socket) {
+        //mapList.put(str, socket);
         socketList.add(socket);
     }
 
-    public List<Socket> getSocketList() {
-        return socketList;
-    }
+     public List<Socket> getSocketList() {
+         return socketList;
+     }
+  /*  public Map<String, Socket> getSocketList() {
+        return mapList;
+    }*/
 
     public void shutdown() {
         running = false;
@@ -31,6 +29,38 @@ public abstract class AbstractOutputThread implements Runnable {
     @Override
     public void run() {
         Scanner scanner = new Scanner(System.in);
+ /*       while (running) {
+            String str = scanner.nextLine();
+            for (Map.Entry<String, Socket> entry : mapList.entrySet()) {
+                Socket socket = entry.getValue();
+                String name =entry.getKey();
+                DataOutputStream os = null;
+                try {
+                    os = new DataOutputStream(socket.getOutputStream());
+                    os.writeUTF(str);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.out.println("关闭输出连接" + socket.getLocalPort());
+                    System.out.println("size=" + mapList.size());
+                    mapList.remove(name);
+                    System.out.println("size=" + mapList.size());
+                    if (os != null) {
+                        try {
+                            os.close();
+                        } catch (IOException e1) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (socket != null) {
+                        try {
+                            socket.close();
+                        } catch (IOException e1) {
+                            e1.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }*/
         while (running) {
             String str = scanner.nextLine();
             Iterator<Socket> it = socketList.iterator();
@@ -41,9 +71,11 @@ public abstract class AbstractOutputThread implements Runnable {
                     os = new DataOutputStream(socket.getOutputStream());
                     os.writeUTF(str);
                 } catch (IOException e) {
-                    //e.printStackTrace();
-                    System.out.println("关闭输出连接");
+                    e.printStackTrace();
+                    System.out.println("关闭输出连接" + socket.getLocalPort());
+                    System.out.println("size=" + socketList.size());
                     it.remove();
+                    System.out.println("size=" + socketList.size());
                     if (os != null) {
                         try {
                             os.close();
