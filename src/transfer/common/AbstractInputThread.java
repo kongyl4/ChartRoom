7bc978e8a4a1390c23b65cce06ad9835d48a438f
kongyl4 <310542914@qq.com>
2016-10-17 18:40:34 +0800

    multi broadcast

--- src/transfer/common/AbstractInputThread.java
+++ src/transfer/common/AbstractInputThread.java
@@ -5,28 +5,27 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
-/**
- * Created by wyx on 2016/10/14.
- */
+
 public class AbstractInputThread implements Runnable {
     private final Socket socket;
 
 
-    private volatile boolean running = true;
+    private  boolean running = true;
     LinkedList<Socket> list = new LinkedList<Socket>();
 
+   // Map<String,Socket> map=new HashMap<String, Socket>();
     public AbstractInputThread(Socket socket) {
         this.socket = socket;
     }
 
+    /*public void addSocket(Socket socket) {
+        list.add(socket);
+    }*/
     public void addSocket(Socket socket) {
         list.add(socket);
     }
-
     public void shutdown() {
         running = false;
     }
@@ -38,15 +37,20 @@ public class AbstractInputThread implements Runnable {
             is = new DataInputStream(socket.getInputStream());
             while (running) {
                 String str = is.readUTF();
-                //1 asdasd
-                //dsadsa
+//密语
                 if (!list.isEmpty()) {
                     String[] arr = str.split("\\s+");
                     int index = 0;
-                    if (arr.length == 2) {
+                   if (arr.length == 2) {
                         index = Integer.valueOf(arr[0]);
                     }
-                    if (index == 0) {
+                    /*if (arr[0].equals(0)) {
+                        for(Map.Entry<String,Socket> entry:map.entrySet()){
+                            Socket socket=entry.getValue();
+                            DataOutputStream os = new DataOutputStream(socket.getOutputStream());
+                            os.writeUTF(str);
+                        }*/
+                    if (index==0) {
                         Iterator<Socket> it = list.iterator();
                         while (it.hasNext()) {
                             Socket socket = it.next();
@@ -55,6 +59,7 @@ public class AbstractInputThread implements Runnable {
                         }
                     } else {
                         Socket socket = list.get(index - 1);
+                        //Socket socket=map.get(arr[0]);
                         DataOutputStream os = new DataOutputStream(socket.getOutputStream());
                         os.writeUTF(str);
                     }
@@ -62,9 +67,10 @@ public class AbstractInputThread implements Runnable {
                 System.out.println(str);
             }
         } catch (IOException e) {
-            //e.printStackTrace();
+            e.printStackTrace();
         } finally {
             System.out.println("关闭输入连接");
+
             if (is != null) {
                 try {
                     is.close();
